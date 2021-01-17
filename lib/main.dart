7d20ddcbbf5893c9c4b82441986edafa534bf6ef
Mr_BananaPants <joran.vancoillie@gmail.com>
2021-01-17 18:00:53 +0100

    design update

--- lib/main.dart
+++ lib/main.dart
@@ -26,43 +26,117 @@ class MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.teal,
         appBar: AppBar(
           title: Text("StarTracker"),
         ),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Row(
+            Align(
+              child: Container(
+                margin: EdgeInsets.fromLTRB(
+                  10,
+                  10,
+                  0,
+                  0,
+                ),
+                child: Text(
+                  'Status:',
+                  style: TextStyle(fontSize: 25),
+                ),
+                alignment: Alignment.topLeft,
+              ),
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceEvenly,
+              buttonHeight: 100.0,
+              buttonMinWidth: 150.0,
               children: [
                 RaisedButton(
-                  child: Text('Aan'),
+                  child: Text(
+                    'Aan',
+                    style: TextStyle(fontSize: 20),
+                  ),
                   onPressed: buttonPressed,
                 ),
                 RaisedButton(
-                  child: Text('Uit'),
+                  child: Text(
+                    'Uit',
+                    style: TextStyle(fontSize: 20),
+                  ),
                   onPressed: buttonPressed,
                 ),
               ],
             ),
-            Row(
+            Align(
+              child: Container(
+                margin: EdgeInsets.fromLTRB(
+                  10,
+                  30,
+                  0,
+                  0,
+                ),
+                child: Text(
+                  'Snelheid:',
+                  style: TextStyle(fontSize: 25),
+                ),
+                alignment: Alignment.topLeft,
+              ),
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceEvenly,
+              buttonHeight: 100.0,
+              buttonMinWidth: 150.0,
               children: [
                 RaisedButton(
-                  child: Text('Traag'),
+                  child: Text(
+                    'Traag',
+                    style: TextStyle(fontSize: 20),
+                  ),
                   onPressed: buttonPressed,
                 ),
                 RaisedButton(
-                  child: Text('Snel'),
+                  child: Text(
+                    'Snel',
+                    style: TextStyle(fontSize: 20),
+                  ),
                   onPressed: buttonPressed,
                 ),
               ],
             ),
-            Row(
+            Align(
+              child: Container(
+                margin: EdgeInsets.fromLTRB(
+                  10,
+                  30,
+                  0,
+                  0,
+                ),
+                child: Text(
+                  'Richting:',
+                  style: TextStyle(fontSize: 25),
+                ),
+                alignment: Alignment.topLeft,
+              ),
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceEvenly,
+              buttonHeight: 100.0,
+              buttonMinWidth: 150.0,
               children: [
                 RaisedButton(
-                  child: Text('Omhoog'),
+                  child: Text(
+                    'Omhoog',
+                    style: TextStyle(fontSize: 20),
+                  ),
                   onPressed: buttonPressed,
                 ),
                 RaisedButton(
-                  child: Text('Omlaag'),
+                  child: Text(
+                    'Omlaag',
+                    style: TextStyle(fontSize: 20),
+                  ),
                   onPressed: buttonPressed,
                 ),
               ],
