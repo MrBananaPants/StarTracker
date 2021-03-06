0068e874b3fd7e35c2bd1fa3a93c4f7fc9ddc6d8
MrBananaPants <joran.vancoillie@gmail.com>
2021-03-06 09:47:56 +0100

    Fixed dark theme icon style

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,6 @@ void main() {
           iconTheme: IconThemeData(color: Colors.black),
           appBarTheme: AppBarTheme(color: Color(0xFFf3f3f8)),
           bottomAppBarColor: Colors.white,
-          // outlinedButtonTheme: OutlinedButtonThemeData(style: ButtonStyle( ),),
           elevatedButtonTheme: ElevatedButtonThemeData(
               style: ElevatedButton.styleFrom(
                   primary: Color(0xFF3D5AFE), shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(7))))),
@@ -50,6 +49,11 @@ void main() {
         iconTheme: IconThemeData(color: Colors.white),
         appBarTheme: AppBarTheme(color: Color(0xFF2b2b2b)),
         bottomAppBarColor: Color(0xFF454545),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style:
+                ElevatedButton.styleFrom(primary: Color(0xFF3D5AFE), shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(7))))),
+        outlinedButtonTheme:
+            OutlinedButtonThemeData(style: ElevatedButton.styleFrom(shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(7))))),
         buttonTheme: ButtonThemeData(buttonColor: Color(0xFF3D5AFE), shape: RoundedRectangleBorder(), textTheme: ButtonTextTheme.primary),
         floatingActionButtonTheme: FloatingActionButtonThemeData(backgroundColor: Color(0xFF3D5AFE)),
       ),
