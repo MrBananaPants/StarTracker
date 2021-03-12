d4aeca255c35735d4cc3534bd9e41b5f5c478c2f
MrBananaPants <joran.vancoillie@gmail.com>
2021-03-12 09:24:21 +0100

    Version bump

--- lib/AboutPage.dart
+++ lib/AboutPage.dart
@@ -106,7 +106,7 @@ class MyAppState extends State<AboutPage> {
                       Padding(
                         padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
                         child: Text(
-                          "Versie: v0.7-beta",
+                          "Versie: v1.0.0",
                           style: TextStyle(
                             color: Theme.of(context).textTheme.bodyText1.color,
                           ),
